7499b0d1e31ec1d86387ea9c11b94a1cec350a03
Henry <60916302+hyknn@users.noreply.github.com>
2022-05-24 23:24:01 +0700

    Update Users Livewire

---
+++ app/Http/Livewire/SearchUsers.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\User;
+
+class SearchUsers extends Component
+{
+    public $search;
+    protected $queryString = ['search'];
+
+    public function render()
+    {
+        return view('livewire.search-users',[
+            'users' => User::where('name', 'like', '%'.$this->search.'%')->get(),
+        ]);
+    }
+}

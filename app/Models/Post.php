153e35f3e89d65ff617dc2f726200911206a254a
Henry <60916302+hyknn@users.noreply.github.com>
2022-05-26 00:05:18 +0700

    Remove tag

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,9 +19,4 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
-
-    public function tags()
-    {
-        return $this->belongsToMany(Tag::class);
-    }
 }

4db75573869aa4ddbef0a3926b6034b769cc5a3c
Henry <60916302+hyknn@users.noreply.github.com>
2022-05-28 14:23:19 +0700

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,14 +1,16 @@
 <div class="container my-5">
     <div class="row">
         <div class="card">
-            <img src="{{ asset('storage/app/public/'.$post->cover) }}" alt="" class="img-fluid">
+            <img src="{{ Storage::url($post->cover) }}" alt="" class="img-fluid">
             <div class="card-body">
                 <h1 class="card-title">{{ $post->title }}</h1>
                 <div class="d-flex my-2">
                     <small class="text-muted">by {{ $post->user->name }} ・ {{
                         Carbon\Carbon::parse($post->created_at)->isoFormat('D MMMM Y'); }}</small>
                 </div>
-                <p>{{ $post->desc }}</p>
+                <div>
+                    {!! $post->desc !!}
+                </div>
                 <div class="card-footer bg-transparent d-flex mx-auto">
                     <a href="{{ route('category',$post->category->slug) }}" class="text-dark">{{ $post->category->name
                         }}</a>
